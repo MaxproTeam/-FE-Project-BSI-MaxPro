c280f072bbd26f5fc8c44c45b72b7d13c85e0393
ferrol143 <ferrolazkimashudi@gmail.com>
2024-09-29 18:57:13 +0700

    updated : fix  code set Attendance

--- resources/js/fetch/AuthJS.js
+++ resources/js/fetch/AuthJS.js
@@ -1,6 +1,6 @@
 const loginAccount = async (data) => {
   try {
-    const response = await axios.post('https://943a-180-251-181-15.ngrok-free.app/api/v1/auth/login', data, {
+    const response = await axios.post('http://localhost:3000/api/v1/auth/login', data, {
       headers: {
         'Content-Type': 'application/json'
       },
