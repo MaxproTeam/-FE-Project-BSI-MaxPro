9f77deea07608b56c07b34a00ae6e567f8ae8793
ferrol143 <ferrolazkimashudi@gmail.com>
2024-09-29 13:48:50 +0700

    added: testing vercel app#2

--- resources/js/fetch/AuthJS.js
+++ resources/js/fetch/AuthJS.js
@@ -1,6 +1,6 @@
 const loginAccount = async (data) => {
   try {
-    const response = await axios.post('http://192.168.1.14:3000/api/v1/auth/login', data, {
+    const response = await axios.post('http://180.251.181.15:3000/api/v1/auth/login', data, {
       headers: {
         'Content-Type': 'application/json'
       },
