70309a8cd8b3f58f9f1a915bf89ad5cbd023e1a8
ferrol143 <ferrolazkimashudi@gmail.com>
2024-10-14 23:06:18 +0700

    ngrok 1

--- resources/js/fetch/picJS.js
+++ resources/js/fetch/picJS.js
@@ -1,6 +1,6 @@
 const getPICAttendance = async (data) => {
     try {
-      const response = await axios.get('https://88fc-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/attedances', {
+      const response = await axios.get('https://a584-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/attedances', {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'x-requested-with': 'XMLHttpRequest'
@@ -19,7 +19,7 @@ const getPICAttendance = async (data) => {
 
   const setPICAttendance = async (data) => {
     try {
-      const response = await axios.post('https://88fc-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/attedances', data, {
+      const response = await axios.post('https://a584-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/attedances', data, {
       headers: {
           'Content-Type': 'application/json',
           'x-requested-with': 'XMLHttpRequest'
@@ -37,7 +37,7 @@ const getPICAttendance = async (data) => {
 
   const getWorkOrders = async (data) => {
     try {
-      const response = await axios.get(`https://88fc-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/work-orders`, {
+      const response = await axios.get(`https://a584-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/work-orders`, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'x-requested-with': 'XMLHttpRequest'
@@ -56,7 +56,7 @@ const getPICAttendance = async (data) => {
 
   const doneWorkOrder = async (data) => {
     try {
-      const response = await axios.put(`https://88fc-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/work-orders`, data, {
+      const response = await axios.put(`https://a584-2001-448a-2020-5ace-e0e6-d412-2525-a3f4.ngrok-free.app/api/v1/pic/work-orders`, data, {
       headers: {
           'Content-Type': 'application/json',
           'x-requested-with': 'XMLHttpRequest'
